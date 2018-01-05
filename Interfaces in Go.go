82acb3fe9d6e3dc2f0fb74b8bd5df522540951fc
Alex <34424602+alexuserid@users.noreply.github.com>
2018-01-05 10:48:32 +0300

    Update Interfaces in Go.go

--- Interfaces in Go.go
+++ Interfaces in Go.go
@@ -1,13 +1,12 @@
-
-/*
-For example, there is an outlet that allows to plug in many different devices:
+/*For example, an outlet allows plugging many different devices: 
 computer, iron, phone charger etc. All because it has universal type of socket (interface).
-So interface (set of methods) in Go allows to use many different implementations of structs
+So interface (set of methods) in Go allows to use many different implementations of structs 
 which have the same interface, that includes the same methods.
-If all plugs (or structs) wouldn't use the same interfaces 
-we would have to use different sockets (or sets of methods)
-every time we want to plug in another one device (or struct),
-even if it's completely idenical.*/
+If no plugs (or structs) would use the same interfaces 
+we would have to use different sockets (or sets of methods) 
+every time we want to plug in another one device (or struct), 
+even if it's completely identical.*/
+
 
 package main 
 
@@ -30,22 +29,76 @@ type Device interface {
   plugIn() string
 }
 
-//creating a new function that prints d by methods that interface includes
-//(or doing something else by interface's methods in another case).
-//Thus we haven't to write some identical functions to print our d that way
-//It's really helpful when your project is bigger
+//creating a new function that prints d by methods that interface includes  
+//Thus, we haven’t to write some identical functions to print our d that way. 
+//It's really helpful when your project is bigger.
 func plug(d Device) {
   println(d.plugIn())
 }
 
 
-//function Plug works with d1 - DeviceComp type variable
-//Plug execute following things:
-//почему мы не могли в функции Plug просто написать println(d)? Ведь d в данном случае вроде как имеет тип интерфейса, 
-//в котором уже есть функция PlugIn. Почему нам надо ее еще и через точку писать? 
-//Следующий комментарий верно отвечает на этот вопрос?
-//println(d1.PlugIn()). Here us a variable d1 that has intrface that call method PlugIn by dot. 
-//Since d1 has type DeviceComp therefore it call method which return "Device 1"
+//function Plug works with d1 - DeviceComp type variable 
+//plug executes following things: 
+//println(d1.plugIn()). Here is a variable d1 that has interface that calls method plugIn by dot. 
+//Since d1 has type DeviceComp therefore it calls method which return "Device 1"
+func main() {
+  var d1 Device = new(DeviceComp)
+  plug(d1)
+
+  var d2 Device = new(DeviceIron)
+  plug(d2)
+}
+
+
+
+
+
+
+/*------------ Русская версия ---------------*/
+/*Например, розетки позволяют подключать к ним самые разные устройства:
+компьютеры, утюги, зарядки для телефонов и т.д. Все потому, 
+что у розетки универсальный тип разъема (интерфейс).
+В Go интерфейс (он же набор методов) позволяет использовать различные реализации структур,
+имеющих одинаковый интерфейс, который включает в себя одни методы.
+Если бы все наши вилки (или структуры Go) имели разные интерфейсы,
+нам бы пришлось использовать разные разъемы розеток
+каждый раз, когда мы хотим подключить какое-то другое устройство с вилкой (или структуру в Go),
+даже если оно неотличимо от другого.*/
+
+
+package main 
+
+//создание новой структуры
+type DeviceComp struct {}  
+//создание нового метода
+func (d *DeviceComp) plugIn() string {   
+  return "Device 1"
+}
+
+//создание еще одной структуры
+type DeviceIron struct {}
+//создание еще одного метода
+func (d *DeviceIron) plugIn() string {
+  return "Device 2"
+}
+
+//создание интерфейса с набором методов, который могут используют идентичные структуры
+type Device interface {
+  plugIn() string
+}
+ 
+//Создание новой функции, выводящей d с помощью включенных в наш интерфейс методов.
+//Таким образом, нам не надо писать кучу одинаковых функций для вывода d таким образом.
+//Все это очень кстати когда приходится иметь дело с большими проектами.
+func plug(d Device) {
+  println(d.plugIn())
+}
+
+
+//Здесь функция Plug работает с d1 - переменной типа DeviceComp
+//plug выполняет следующие действия:
+//println(d1.plugIn()). Здесь мы видим переменную d1, имеющую интерфейс, вызывающий метод plugIn через точку.
+//Так как d1 имеет тип DeviceComp, следовательно вызывается метод, возвращающий "Device 1"
 func main() {
   var d1 Device = new(DeviceComp)
   plug(d1)
