8c0dc4e73120a65c0e4d0ea465300337799d3814
Alex <34424602+alexuserid@users.noreply.github.com>
2018-01-17 19:46:16 +0300

    Update pointers.go

--- pointers.go
+++ pointers.go
@@ -7,9 +7,6 @@ func main() {
 	var n int = 6 
 	var pn *int //pn == <nil>, &pn == mA2
 
-	// *pn = 3
-	// n = *pn
-
 	pn = &n //n == 6, &n == mA1, *pn == 6, &pn == mA2
 	n = 7 //n == 7, &n == mA1, pn == mA1, *pn == 7, &pn == mA2
 	*pn = 3 //n == 3, &n == mA1, pn == mA1, &pn == mA2, *pn == 3
