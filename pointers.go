d18cf9060527ecb9381ac19e75d3c2d2013197ab
Alex <34424602+alexuserid@users.noreply.github.com>
2018-01-17 19:58:00 +0300

    Update pointers.go

--- pointers.go
+++ pointers.go
@@ -3,10 +3,10 @@ package pointers
 
 //mA == memory adress
 func main() {
-	var n int = 6 //n == 6, & == mA1
+	var n int //n == 0, &n == mA1
 	var pn *int //pn == <nil>, &pn == mA2
 
-	pn = &n //n == 6, &n == mA1, *pn == 6, &pn == mA2
+	pn = &n //n == 0, &n == mA1, *pn == 0, &pn == mA2
 	n = 7 //n == 7, &n == mA1, pn == mA1, *pn == 7, &pn == mA2
 	*pn = 3 //n == 3, &n == mA1, pn == mA1, &pn == mA2, *pn == 3
 	n = *pn //n == 3, &n == mA1, pn == mA1, &pn == mA2, *pn == 3
