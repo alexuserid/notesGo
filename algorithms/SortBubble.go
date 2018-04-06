f6450aab7d36c0dbea7dec7a3afb1b8966cfaf36
A <alexuserid@gmail.com>
2018-04-07 00:21:24 +0300

    modify SortBubble.go

--- algorithms/SortBubble.go
+++ algorithms/SortBubble.go
@@ -2,22 +2,19 @@ package main
 
 import "fmt"
 
-func Bubble(data []string) []string {
-	var tmp string
+func Bubble(data []string) {
 
 	for i := len(data) - 1; i > 0; i-- {
 		for j := 0; j < i; j++ {
 			if data[j] > data[j+1] {
-				tmp = data[j]
-				data[j] = data[j+1]
-				data[j+1] = tmp
+				data[j], data[j+1] = data[j+1], data[j]
 			}
 		}
 	}
-	return data
 }
 
 func main() {
 	data := []string{"f", "e", "d", "c", "b", "a"}
-	fmt.Println(Bubble(data))
+	Bubble(data)
+	fmt.Println(data)
 }
